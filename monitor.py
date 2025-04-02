b204a1630ce3d7334005a0fa2956df0c8ef8f146
Luca Söthe <soethe@embedded.rwth-aachen.de>
2025-04-02 11:00:25 +0200

    Fix issues

--- monitor.py
+++ monitor.py
@@ -29,18 +29,20 @@ def append_and_return(li, item):
     return item
 
 class Monitor:
-    def __initFN(self, phi, step=0):
+    def __initFN(self, phi, step=0, update_or_insert=None):
+        if update_or_insert == None:
+            update_or_insert = lambda x: append_and_return(self.requests, x)
         appended = []
         if isinstance(phi, AP):
-            appended.append(append_and_return(self.requests, MonitoredFormula(phi, "", step)))
+            appended.append(update_or_insert(MonitoredFormula(phi, "", step)))
         elif isinstance(phi, Not):
-            appended.append(append_and_return(self.requests, MonitoredFormula(phi, "", step, self.__initFN(phi.children[0], step))))
+            appended.append(update_or_insert(MonitoredFormula(phi, "", step, self.__initFN(phi.children[0], step, update_or_insert))))
         elif isinstance(phi, BinaryOperator):
-            appended.append(append_and_return(self.requests, MonitoredFormula(phi, "", step, self.__initFN(phi.children[0], step) + self.__initFN(phi.children[1], step))))
+            appended.append(update_or_insert(MonitoredFormula(phi, "", step, self.__initFN(phi.children[0], step, update_or_insert) + self.__initFN(phi.children[1], step, update_or_insert))))
         elif isinstance(phi, G) or isinstance(phi, F):
-            appended.append(append_and_return(self.requests, MonitoredFormula(phi, "", step, self.__initFN(phi.children[0], step))))
+            appended.append(update_or_insert(MonitoredFormula(phi, "", step, self.__initFN(phi.children[0], step, update_or_insert))))
         elif isinstance(phi, X) or isinstance(phi, W):
-            appended.append(append_and_return(self.requests, MonitoredFormula(phi, "", step)))
+            appended.append(update_or_insert(MonitoredFormula(phi, "", step)))
         else:
             raise ValueError("Invalid formula")
         return appended
@@ -51,9 +53,9 @@ class Monitor:
         self.evaluations = []
         self.__initFN(formula)
 
-    def __get_eval(self, formula):
+    def __get_eval(self, req, formula):
         for eval in self.evaluations:
-            if eval.formula.formula == formula:
+            if eval.formula in req.depends_on and eval.formula.formula == formula:
                 return eval.verdict
         raise ValueError("Evaluation not found")
 
@@ -74,26 +76,34 @@ class Monitor:
 
     def step(self, observations):
         # Apply evaluation rules
+        # import ipdb; ipdb.set_trace()
         for req in self.requests:
             if isinstance(req.formula, AP):
                 self.__insert_or_replace_eval(Evaluation(req, (Verdict.TRUE if req.formula.name in observations else Verdict.FALSE, '')))
             elif isinstance(req.formula, BinaryOperator):
                 eval_table = eval_tables[type(req.formula)][req.mode]
                 if req.mode == "L":
-                    self.__insert_or_replace_eval(Evaluation(req, eval_table[self.__get_eval(req.formula.children[0])[0]]))
+                    self.__insert_or_replace_eval(Evaluation(req, eval_table[self.__get_eval(req, req.formula.children[0])[0]]))
                 elif req.mode == "R":
-                    self.__insert_or_replace_eval(Evaluation(req, eval_table[self.__get_eval(req.formula.children[1])[0]]))
+                    self.__insert_or_replace_eval(Evaluation(req, eval_table[self.__get_eval(req, req.formula.children[1])[0]]))
                 else:
-                    self.__insert_or_replace_eval(Evaluation(req, eval_table[self.__get_eval(req.formula.children[0])[0]][self.__get_eval(req.formula.children[1])[0]]))
+                    self.__insert_or_replace_eval(Evaluation(req, eval_table[self.__get_eval(req, req.formula.children[0])[0]][self.__get_eval(req, req.formula.children[1])[0]]))
             elif isinstance(req.formula, (X, W)):
                 eval_table = eval_tables[type(req.formula)][req.mode]
                 if req.mode == '':
                     self.__insert_or_replace_eval(Evaluation(req, eval_tables[type(req.formula)][req.mode]))
                 else:
-                    self.__insert_or_replace_eval(Evaluation(req, eval_table[self.__get_eval(req.formula.children[0])[0]]))
+                    self.__insert_or_replace_eval(Evaluation(req, eval_table[self.__get_eval(req, req.formula.children[0])[0]]))
+            elif isinstance(req.formula, G):
+                self.__insert_or_replace_eval(Evaluation(req, ((Verdict.UNKNOWN_TRUE if all(map(lambda x: self.__get_eval(req, x.formula)[0] == Verdict.TRUE, req.depends_on))
+                                                              else (Verdict.FALSE if any(map(lambda x: self.__get_eval(req, x.formula)[0] == Verdict.FALSE, req.depends_on))
+                                                                    else Verdict.UNKNOWN_FALSE)), "")))
+            elif isinstance(req.formula, F):
+                self.__insert_or_replace_eval(
+                    Evaluation(req, (Verdict.TRUE if any(map(lambda x: self.__get_eval(req, x.formula)[0] == Verdict.TRUE, req.depends_on)) else Verdict.UNKNOWN_FALSE), ""))
             elif isinstance(req.formula, UnaryOperator):
                 eval_table = eval_tables[type(req.formula)][req.mode]
-                self.__insert_or_replace_eval(Evaluation(req, eval_table[self.__get_eval(req.formula.children[0])[0]]))
+                self.__insert_or_replace_eval(Evaluation(req, eval_table[self.__get_eval(req, req.formula.children[0])[0]]))
             else:
                 raise ValueError("Invalid formula")
 
@@ -103,52 +113,60 @@ class Monitor:
         def update_or_insert(f):
             if f not in old_requests:
                 self.requests.add(f)
+                return f
             else:
                 for req in old_requests:
                     if req == f:
                         self.requests.add(req)
                         req.mode = f.mode
                         req.depends_on = f.depends_on
-                        break
+                        return req
         for eval in self.evaluations:
             if eval.verdict[0] == Verdict.UNKNOWN_TRUE or eval.verdict[0] == Verdict.UNKNOWN_FALSE:
                 if isinstance(eval.formula.formula, Not):
                     update_or_insert(MonitoredFormula(eval.formula.formula, "", eval.formula.step, eval.formula.depends_on))
                 elif isinstance(eval.formula.formula, (And, Or)):
+                    # import ipdb; ipdb.set_trace()
                     update_or_insert(MonitoredFormula(eval.formula.formula, eval.verdict[1], eval.formula.step, eval.formula.depends_on))
                 elif isinstance(eval.formula.formula, (G, F)):
-                    new_inits = self.__initFN(eval.formula.formula.children[0], eval.formula.step + 1)
-                    update_or_insert(MonitoredFormula(eval.formula.formula, "", eval.formula.step, eval.formula.depends_on.union(new_inits)))
+                    new_inits = self.__initFN(eval.formula.formula.children[0], eval.formula.step + 1, update_or_insert)
+                    update_or_insert(MonitoredFormula(eval.formula.formula, "", eval.formula.step + 1, eval.formula.depends_on.union(new_inits)))
                 elif isinstance(eval.formula.formula, (X, W)):
                     depends = eval.formula.depends_on
                     if eval.formula.mode == "":
                         depends = depends.union(self.__initFN(eval.formula.formula.children[0], eval.formula.step + 1))
                     update_or_insert(MonitoredFormula(eval.formula.formula, eval.verdict[1], eval.formula.step, depends))
                 elif isinstance(eval.formula.formula, U):
-                    self.__initFN(eval.formula.formula.children[0])
-                    self.__initFN(eval.formula.formula.children[1])
+                    self.__initFN(eval.formula.formula.children[0], 0, update_or_insert)
+                    self.__initFN(eval.formula.formula.children[1], 0, update_or_insert)
                     update_or_insert(MonitoredFormula(eval.formula.formula, eval.verdict[1]))
                 else:
                     raise ValueError("Invalid formula")
 
+def rep_op(op, num):
+    if num == 1:
+        return op
+    else:
+        def inner(x):
+            return rep_op(op, num - 1)(op(x))
+        return inner
+        
+
 if __name__ == "__main__":
-    phi = G(Or(AP('a'), X(AP('b'))))
+    phi = G(Or(AP('a'), rep_op(X, 1)(AP('b'))))
     print(phi)
     mon = Monitor(phi)
     print(map(str, mon.requests))
     n = 0
 
-    for aps in [[], ['b'], ['a', 'b']]:
+    for aps in [[], ['a', 'b'], ['a']]:
         print(aps)
         dg = mon.to_dot()
         dg.name = "Step " + str(n)
         dg.view(directory="/tmp")
-        raw_input()
+#        raw_input("Press Enter to continue...")
         mon.step(aps)
         print(map(lambda x: str(x.formula) + ": " + str(x.verdict[0]) + x.verdict[1], mon.evaluations))
         print(map(str, mon.requests))
         print()
         n += 1
-    print(map(lambda x: str(x.formula) + ": " + str(x.verdict[0]) + x.verdict[1], mon.evaluations))
-    print(map(str, mon.requests))
-    print()
